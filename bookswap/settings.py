fcf4d18b94ff779b4b6422fb1e1a377e5a1eb0a0
Caio Santos <c.santosggwp@gmail.com>
2024-09-25 13:34:43 -0300

    deploy: update docker to google cloud

--- bookswap/settings.py
+++ bookswap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4z1a90mk6@#embe6-)^!v5rbbvmy3bcgv&euhni6(p58l#iom0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.15.10','127.0.0.1','0.0.0.0','10.10.31.132','10.90.8.67','bookswap-backend-production.up.railway.app']
+ALLOWED_HOSTS = ['localhost','192.168.15.10','127.0.0.1','0.0.0.0','10.10.31.132','10.90.8.67','bookswap-backend-production.up.railway.app']
 
 
 # Application definition
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'bookswap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookswap',
+        'USER': 'root',
+        'PASSWORD': 'ps2024bk',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
