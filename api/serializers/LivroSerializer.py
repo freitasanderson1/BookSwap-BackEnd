26c6fde030e458450647e7b71923f02f5cb1e4f1
CaioSantDev <c.santosggwp@gmail.com>
2024-09-28 22:50:18 -0300

    fix: formato de dadas dia-mes-ano

--- api/serializers/LivroSerializer.py
+++ api/serializers/LivroSerializer.py
@@ -3,6 +3,7 @@ from api.models import Livro
 
 class LivroSerializer(serializers.ModelSerializer):
     dono = serializers.CharField(source='dono.username',read_only=True )
+    dataPublicacao = serializers.DateField(format="%d/%m/%Y")
     class Meta:
         model = Livro
         fields = ['url','id','titulo','autor','paginas','descricao','dataPublicacao','editora','capa','dono','condicao','genero']
--- bookswap/settings.py
+++ bookswap/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4z1a90mk6@#embe6-)^!v5rbbvmy3bcgv&euhni6(p58l#iom0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','192.168.15.10','127.0.0.1','0.0.0.0','10.10.31.132','10.90.8.67','bookswap-backend-production.up.railway.app']
+ALLOWED_HOSTS = ['192.168.15.4','192.168.15.10','127.0.0.1','0.0.0.0','10.10.31.132','10.90.8.67','bookswap-backend-production.up.railway.app']
 
 
 # Application definition
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DATE_FORMAT': "%d/%m/%Y",
+    'DATE_INPUT_FORMATS': ["%d-%m-%Y"],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
