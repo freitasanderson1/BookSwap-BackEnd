e2c6dbd49dd65f838b1d3f1a49c9a3450e65e889
Caio Santos <c.santosggwp@gmail.com>
2024-09-30 10:51:33 -0300

    test deploy

--- bookswap/settings.py
+++ bookswap/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4z1a90mk6@#embe6-)^!v5rbbvmy3bcgv&euhni6(p58l#iom0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.15.118','192.168.15.4','192.168.15.10','127.0.0.1','0.0.0.0','10.10.31.132','10.90.8.67','bookswap-backend-production.up.railway.app']
+ALLOWED_HOSTS = ['192.168.15.118','192.168.15.4','192.168.15.10','0f73-200-129-179-189.ngrok-free.app','0.0.0.0','10.10.31.132','10.90.8.67','bookswap-backend-production.up.railway.app']
 
 
 # Application definition
