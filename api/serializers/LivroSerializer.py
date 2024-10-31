a937049e3b5afc45efeb901146db57c6e00e2fb9
Laura <laurahenrique9@gmail.com>
2024-10-30 23:16:30 -0300

    editar livro

--- api/serializers/LivroSerializer.py
+++ api/serializers/LivroSerializer.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from api.models import Livro
+from datetime import datetime
 
 class LivroSerializer(serializers.ModelSerializer):
-    dono = serializers.CharField(source='dono.username',read_only=True )
-    dataPublicacao = serializers.DateField(format="%d/%m/%Y")
+    dono = serializers.CharField(source='dono.username', read_only=True)
     perfil_id = serializers.IntegerField(source='dono.perfil.id', read_only=True)  # Access the related Perfil's ID
+    dataPublicacao = serializers.DateField(format="%d/%m/%Y", input_formats=['%d-%m-%Y', '%d/%m/%Y'])
+
     class Meta:
         model = Livro
-        fields = ['url','id','titulo','autor','paginas','descricao','dataPublicacao','editora','capa','dono','perfil_id','condicao','genero','resenha','curtidas']
+        fields = ['url', 'id', 'titulo', 'autor', 'paginas', 'descricao', 'dataPublicacao', 'editora', 'capa', 'dono', 'perfil_id', 'condicao', 'genero', 'resenha', 'curtidas']
--- api/views/LivroViewSet.py
+++ api/views/LivroViewSet.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from api.serializers import LivroSerializer
 from api.filters.LivroFilter import LivroFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.exceptions import PermissionDenied
 
 class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all().order_by('-criado_em')
@@ -15,23 +16,26 @@ class LivroViewSet(viewsets.ModelViewSet):
     search_fields = ['titulo', 'condicao']
 
     def get_permissions(self):
-        """Customiza as permissões por método"""
-        if self.action in ['list', 'retrieve']:  # Se for um GET (listar ou ver um livro), é público
+        if self.action in ['list', 'retrieve']:
             permission_classes = [AllowAny]
-        else:  # Para qualquer outra ação (POST, PUT, DELETE, etc.), exige autenticação
+        else:
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
 
     def get_queryset(self):
-        # Se um parâmetro "meus_livros" for passado, filtramos para livros do usuário
         meus_livros = self.request.query_params.get('meus_livros', None)
         if meus_livros and self.request.user.is_authenticated:
             return Livro.objects.filter(dono=self.request.user).order_by('-criado_em')
         return super().get_queryset()
 
     def perform_create(self, serializer):
-        # Associa o livro criado ao usuário autenticado
         serializer.save(dono=self.request.user)
+
+    def get_object(self):
+        obj = super().get_object()
+        if obj.dono != self.request.user:
+            raise PermissionDenied("Você não tem permissão para editar este livro.")
+        return obj
 
     @action(detail=True, methods=['post'], url_path='curtir', url_name='curtir')
     def curtir_livro(self, request, pk=None):
